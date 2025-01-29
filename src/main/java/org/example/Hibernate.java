5d0a23d46ada8edbe9947b97e79c920fea863093
Simsunworld <87257327+Simsunworld@users.noreply.github.com>
2025-01-29 20:17:38 +0300

    added new text to the Hibernate class

---
+++ src/main/java/org/example/Hibernate.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Hibernate
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hibernate" );
+        System.out.println( "Hibernate" );
+        System.out.println( "Hibernate" );
+        System.out.println( "Hibernate" );
+    }
+}
